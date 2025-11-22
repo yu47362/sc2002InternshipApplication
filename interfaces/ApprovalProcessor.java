3eb39db8917cbb264df1101c7853a488643284e9
yu47362 <djy.ac.1129@gmail.com>
2025-11-22 23:39:47 +0800

    Add Javadoc comments to ApprovalProcessor interface
    Added documentation comments to ApprovalProcessor interface.

--- interfaces/ApprovalProcessor.java
+++ interfaces/ApprovalProcessor.java
@@ -3,9 +3,14 @@ package interfaces;
 import models.CompanyRepresentative;
 import models.Student;
 import java.util.List;
-
+/**
+ * This is to call method of approve company rep, approve internship or process withdrawal
+ * @author Dai Jiayu
+ * @version 1.0
+ */
 public interface ApprovalProcessor {
     void approveCompanyRepresentatives(List<CompanyRepresentative> reps);
     void approveInternships(List<CompanyRepresentative> reps);
     void processWithdrawals(List<Student> students);
+
 }
