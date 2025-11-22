202a316b83763f42ff3989b4ada4ef3bbe69026b
yu47362 <djy.ac.1129@gmail.com>
2025-11-22 23:40:12 +0800

    Add Javadoc comments to FilterManager interface
    Added documentation comments to the FilterManager interface.

--- interfaces/FilterManager.java
+++ interfaces/FilterManager.java
@@ -3,9 +3,14 @@ package interfaces;
 import models.InternshipOpportunity;
 import models.InternshipFilter;
 import java.util.List;
-
+/**
+ * This is call on filter method
+ * @author Dai Jiayu
+ * @version 1.0
+ */
 public interface FilterManager {
     void filterInternships(List<InternshipOpportunity> allInternships,
                                     InternshipFilterService filterService,
                                     InternshipFilter currentFilter);
+
 }
