e5ac232ec960e0a77b896ce4f893d6e012cc37c7
yu47362 <djy.ac.1129@gmail.com>
2025-11-22 23:41:48 +0800

    Enhance InternshipViewer with Javadoc comments
    Added Javadoc comments to the InternshipViewer interface.

--- interfaces/InternshipViewer.java
+++ interfaces/InternshipViewer.java
@@ -3,8 +3,13 @@ package interfaces;
 import models.InternshipOpportunity;
 import models.InternshipFilter;
 import java.util.List;
-
+/**
+ * This is to view the filtered internship information
+ * @author Dai Jiayu
+ * @version 1.0
+ */
 public interface InternshipViewer {
     void viewInternshipsWithFilter(List<InternshipOpportunity> internships, InternshipFilter filter);
     String getViewerType();
+
 }
