ae185c8daa9c849fc594949cf4a326e8e6b4e833
yu47362 <djy.ac.1129@gmail.com>
2025-11-22 23:40:43 +0800

    Add Javadoc comments to InternshipFilterService
    Added documentation comments to the InternshipFilterService interface.

--- interfaces/InternshipFilterService.java
+++ interfaces/InternshipFilterService.java
@@ -3,7 +3,11 @@ package interfaces;
 import models.InternshipOpportunity;
 import models.InternshipFilter;
 import java.util.List;
-
+/**
+ * This is to get the internship with matching characteristic
+ * @author Dai Jiayu
+ * @version 1.0
+ */
 public interface InternshipFilterService {
     List<InternshipOpportunity> filterInternships(List<InternshipOpportunity> internships, InternshipFilter filter);
     List<String> getAvailableLevels(List<InternshipOpportunity> internships);
@@ -11,3 +15,4 @@ public interface InternshipFilterService {
     List<String> getAvailableStatuses(List<InternshipOpportunity> internships);
     List<String> getAvailablePreferredMajors(List<InternshipOpportunity> internships);
 }
+
