e5cd799299309569f72d786ff8ffc50ffb1eaf83
yu47362 <djy.ac.1129@gmail.com>
2025-11-22 16:51:58 +0800

    Add method to get available preferred majors

--- interfaces/InternshipFilterService.java
+++ interfaces/InternshipFilterService.java
@@ -9,4 +9,5 @@ public interface InternshipFilterService {
     List<String> getAvailableLevels(List<InternshipOpportunity> internships);
     List<String> getAvailableCompanies(List<InternshipOpportunity> internships);
     List<String> getAvailableStatuses(List<InternshipOpportunity> internships);
+    List<String> getAvailablePreferredMajors(List<InternshipOpportunity> internships);
 }
